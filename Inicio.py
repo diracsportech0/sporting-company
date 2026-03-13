30f3ca3bc15a2d2367c446e0cb34f41e84b7695a
diracsportech0 <dirac.sportech@gmail.com>
2026-03-12 22:38:43 -0500

    add Entrenamientos

--- Inicio.py
+++ Inicio.py
@@ -16,15 +16,34 @@ with colC:
 
 #DATA
 df_matches = pd.read_excel('Matches.xlsx')
+df_entreno = pd.read_excel('Entrenamientos.xlsx')
 
 
 #FORMATO
-st.header(f'Bienvenido SPORTIGNG COMPANY!!')
+st.header(f'Bienvenido SPORTING COMPANY!!')
 
-st.write("Videoteca: Partidos Y Entrenamientos")
+st.write("Videoteca: Partidos & Entrenamientos")
 
-df_matches = df_matches.dropna(subset=['video'])
-df_matches = df_matches[df_matches.Propio==True]
+df_entreno = df_entreno.dropna(subset=['video'])
+
+#-----------FILTRO POR FECHA
+df_entreno['date'] = pd.to_datetime(df_entreno['date'])
+# MES
+meses = df_entreno['date'].dt.to_period('M').unique()
+mes = st.sidebar.selectbox("Mes", meses)
+df_mes = df_entreno[df_entreno['date'].dt.to_period('M') == mes]
+# SEMANA
+semanas = df_mes['date'].dt.isocalendar().week.unique()
+semana = st.sidebar.selectbox("Semana", semanas)
+df_semana = df_mes[df_mes['date'].dt.isocalendar().week == semana]
+# DIA
+dias = df_semana['date'].dt.day_name().unique()
+dia = st.sidebar.selectbox("Día", dias)
+df_final = df_semana[df_semana['date'].dt.day_name() == dia]
+
+#######-------------
+
+'''
 #rivales = df_matches['match_filter'].values
 etapa = set(df_matches['Etapa'].values)
 
@@ -37,10 +56,11 @@ etapa_select = st.sidebar.multiselect(
 #    rivales,
 #    rivales)
 df_matches = df_matches[df_matches['Etapa'].isin(etapa_select)]
-urls_match = df_matches['video'].values
+'''
+urls_match = df_entreno['video'].values
 
 #df_matches = df_matches[df_matches['match_filter'].isin(partidos_select)]
-n_matches = df_matches.shape[0]
+n_matches = df_entreno.shape[0]
 n_columns = 3
 for i in range(0, n_matches, n_columns):
     cols = st.columns(n_columns)
